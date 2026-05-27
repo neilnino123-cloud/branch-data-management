b81415feb9c42813f9a1b2df736713fabd30a3d2
neilnino123-cloud <neilnino123@gmail.com>
2026-05-27 14:35:41 +0800

    Update views.py

--- views.py
+++ views.py
@@ -310,7 +310,7 @@ def render_encoder_view(user):
         if st.button("Submit Data"):
             # Validation
             errors = []
-            if not st.session_state.enc_name == "-- Full Name --":
+            if st.session_state.get("enc_name", "-- Full Name --") == "-- Full Name --":
                 errors.append("Please enter your full name")
             if st.session_state.store_name == "-- Select Customer --":
                 errors.append("Please select a customer")
