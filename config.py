c5a5d2bea44333ed4f072c7f3c3691b2d7109131
neilnino123-cloud <neilnino123@gmail.com>
2026-06-19 08:25:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -90,6 +90,7 @@ SAMAR_ENCODERS = {
                     "ARISTEO ASI",
                     "CLETUS JOMAR PENEDA",
                     "DONALD CABO",
+                    "EDISAR LIMBAWAN",
                     "JOHN LAWRENCE GUASIS",
                     "RALPH CARVAJAL",
                     "REDELON ABARRO",
